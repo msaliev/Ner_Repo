96ce4025b96ca104513508e8b5cb5e7dc0de6437
Muhammad Yusuf <farmax298@gmail.com>
2021-09-21 13:02:03 +0500

    1st Git push

---
+++ main.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /* int i = 1;
+    int j = 2;
+    bool flag = i == j;
+    bool alpha = i > j;
+    bool betta = i < j;
+    bool gamma = i != j;
+    bool delta = i >= j;
+    bool myu = i <= j;
+    cout << boolalpha << alpha;
+    cout << boolalpha << "\n" << betta;
+    cout << boolalpha << "\n" << gamma;
+    cout << boolalpha << "\n" << delta;
+    cout << boolalpha << "\n" << flag;
+    cout << boolalpha << "\n" << myu; */
+
+    /*int a = 49;
+    int b = 121;
+    int c = a ^ b;
+    int k = c ^ b;
+    int j = c ^ a;
+    bool result1 = k == b;
+    bool result = j == a;
+
+    cout << result1;
+    cout << "\n" << result;*/
+    /* int a = 212;
+    cout << (a >> 0);
+    cout << "\n" << (a >> 0);
+   cout << "\n" << (a >> 1);
+   cout << "\n" << (a >> 2);
+   cout << "\n" << (a >> 3);*/
+
+    /*int m, s;
+    cout << "Enter the number in minutes:";
+    cin >> m;
+    s = m * 60;
+    cout << s;*/
+    /*int a, b;
+    char flag;
+    //time form 12:34
+    cout << "Enter the time: ";
+    cin >> a >> flag >> b;
+    cout << a * 3600 + b * 60;*/
+
+    /*int b; //Byte
+    int kb; //KByte
+    cout << "Enter the amount in Byte: ";
+    cin >> b;
+    kb = b >> 10;
+    cout << kb; */
+
+    /*int a;
+    int b;
+    int c;
+
+    cout << "Enter number a: ";
+    cin >> a;
+    cout << "Enter number b: ";
+    cin >> b;
+
+    c = b;
+    b = a;
+    cout << c << "\n";
+    cout << b; */
+
+    /*int a, b;
+    cout << "Enter 2 random numbers: ";
+    cin >> a >> b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << a;
+    cout << "\n" << b; */
+
+    /*int a, b, c;
+    cin >> a >> b;
+     c = a ^ b;
+     a = c ^ a;
+     a = c ^ b;
+     cout << b;
+     cout << "\n" << a;*/
+
+    /*int a, b;
+    cin >> a >> b;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    cout << a;
+    cout << "\n" << b;*/
+
+    /* int a;
+    int b;
+    int c;
+    cout << "Enter the number: ";
+    cin >> a;
+    b = a * a;
+    c = b * b;
+    cout << c; */
+
+    /*int number1, number2, maxNumber, minNumber;
+    cout << "Enter the first number that you want to compare: ";
+    cin >> number1;
+    cout << "Enter the second number that you want to compare: ";
+    cin >> number2;
+    maxNumber = (number1 > number2) * number1 + (number2 > number1) * number2;
+    minNumber = (number1 < number2) * number1 + (number2 < number1) * number2;
+
+
+
+    cout << "The biggest one is: " << maxNumber;
+    cout << "\nThe smallest one is: " << minNumber; */
+
+    /* int a;
+    int b;
+    int c;
+    int d;
+    cout << "Enter the number: ";
+    cin >> a;
+    b = a * a;
+    c = b * b;
+    d = a * b;
+    cout << d; */
+
+    /*int a;
+    int b;
+
+    cout << "Enter the number: ";
+    cin >> a;
+    b = a * a;
+     a = b * b;
+    a = a * b;
+    cout << a; */
+    /*int a, b;
+
+    cout<< "a = "; cin>>a;
+    cout<< "b = "; cin>>b;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout << "\na = "<< a << "\tb = "<< b << endl; */
+
+    /*int a, b;
+    cin >> a;
+    b = a*a*a;
+    a = b * b;
+    a = a * a * b;
+    cout << a ; */
+
+    return 0;
+}
